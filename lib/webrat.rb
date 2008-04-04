a0ea353e4210b43c72ed3063afd72a3dec17432f
Bryan Helmkamp <bryan@brynary.com>
2008-04-04 16:17:28 +0100

    Bump version for eventual next release

--- lib/webrat.rb
+++ lib/webrat.rb
@@ -2,5 +2,5 @@ require File.join(File.dirname(__FILE__), "webrat", "rails_extensions")
 require File.join(File.dirname(__FILE__), "webrat", "session")
 
 class Webrat
-  VERSION = '0.2.0'
+  VERSION = '0.2.1'
 end
