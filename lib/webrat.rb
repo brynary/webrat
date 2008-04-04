9adf2193d211fb80a73a0e2a0ccab2bb5038f3b6
Bryan Helmkamp <bryan@brynary.com>
2008-04-04 17:12:46 +0100

    Use modules for webrat extension

--- lib/webrat.rb
+++ lib/webrat.rb
@@ -1,6 +1,6 @@
 require File.join(File.dirname(__FILE__), "webrat", "rails_extensions")
 require File.join(File.dirname(__FILE__), "webrat", "session")
 
-class Webrat
+module Webrat
   VERSION = '0.2.1'
 end
--- lib/webrat/rails_extensions.rb
+++ lib/webrat/rails_extensions.rb
@@ -1,27 +1,25 @@
+module Webrat
+  module RedirectActions
+    def put_via_redirect(path, parameters = {}, headers = {})
+      put path, parameters, headers
+      follow_redirect! while redirect?
+      status
+    end
+
+    def delete_via_redirect(path, parameters = {}, headers = {})
+      delete path, parameters, headers
+      follow_redirect! while redirect?
+      status
+    end
+  end
+end
+
+# Waiting for http://dev.rubyonrails.org/ticket/10497 to be committed.
+  
 module ActionController
   module Integration
-
     class Session
-      
-      unless instance_methods.include?("put_via_redirect")
-        # Waiting for http://dev.rubyonrails.org/ticket/10497 to be committed.
-        def put_via_redirect(path, parameters = {}, headers = {})
-          put path, parameters, headers
-          follow_redirect! while redirect?
-          status
-        end
-      end
-
-      unless instance_methods.include?("delete_via_redirect")
-        # Waiting for http://dev.rubyonrails.org/ticket/10497 to be committed.
-        def delete_via_redirect(path, parameters = {}, headers = {})
-          delete path, parameters, headers
-          follow_redirect! while redirect?
-          status
-        end
-      end
-
+      include Webrat::RedirectActions unless instance_methods.include?("put_via_redirect")
     end
-    
   end
 end
