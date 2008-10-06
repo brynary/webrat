c536b422dfa0bce50c4bf5709d2a2863ea84d78d
David Chelimsky <dchelimsky@gmail.com>
2008-10-06 06:52:36 -0500

    require 'spec/interop/test' so you can run specs w/ the ruby command

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "rubygems"
 require "spec"
+require "spec/interop/test"
 
 # gem install redgreen for colored test output
 begin require "redgreen" unless ENV['TM_CURRENT_LINE']; rescue LoadError; end
