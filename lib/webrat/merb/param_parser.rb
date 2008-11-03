85f16d2cdc9fcc62fe1d3715fb1ff24903eb7ce2
Yehuda Katz <wycats@gmail.com>
2008-11-02 19:11:20 -0800

    Support Merb 1.0

--- lib/webrat/merb/param_parser.rb
+++ lib/webrat/merb/param_parser.rb
@@ -1,21 +1,14 @@
 require "cgi"
-require "webrat/core_extensions/blank"
-require "webrat/merb/url_encoded_pair_parser"
+gem "extlib"
+require "extlib"
+require "merb-core"
+
+HashWithIndifferentAccess = Mash
 
 module Webrat
   class ParamParser
     def self.parse_query_parameters(query_string)
-      return {} if query_string.blank?
-
-      pairs = query_string.split('&').collect do |chunk|
-        next if chunk.empty?
-        key, value = chunk.split('=', 2)
-        next if key.empty?
-        value = value.nil? ? nil : CGI.unescape(value)
-        [ CGI.unescape(key), value ]
-      end.compact
-
-      UrlEncodedPairParser.new(pairs).result
+      Merb::Parse.query(query_string)
     end
   end
 end
