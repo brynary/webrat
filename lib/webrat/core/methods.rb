5d3cb353701c84e80ce7f256757010dc2c6145b1
Bryan Helmkamp <bryan@brynary.com>
2008-11-06 13:52:52 -0500

    Working on Webrat::Methods

--- lib/webrat/core/methods.rb
+++ lib/webrat/core/methods.rb
@@ -11,31 +11,31 @@ module Webrat
       end
     end
     
-    def self.delegate_to_session_returning_response(*meths)
-      meths.each do |meth|
-        self.class_eval <<-RUBY
-          def #{meth}(*args, &blk)
-            webrat_session.#{meth}(*args, &blk)
-            return webrat_session.response
-          end
-        RUBY
-      end
-    end
-    
     def webrat_session
       @webrat_session ||= ::Webrat::Session.new
     end
 
-    delegate_to_session :within, :header, :http_accept, :basic_auth,
-                        :save_and_open_page, :fill_in, :check, 
-                        :uncheck, :choose, :select, :attach_file,
-                        :field_labeled, :cookies, :response, :current_page,
-                        :current_url
-
-    delegate_to_session_returning_response :visits, :click_link, :click_area, :click_button, :reload, :clicks_link_within
-    
-    alias reloads reload
-    alias visit visits
+    delegate_to_session \
+      :visits, :visit,
+      :within,
+      :header, :http_accept, :basic_auth,
+      :save_and_open_page,
+      :fill_in,
+      :check, 
+      :uncheck,
+      :choose,
+      :select,
+      :attach_file,
+      :cookies,
+      :response,
+      :current_page,
+      :current_url,
+      :click_link,
+      :click_area,
+      :click_button,
+      :reload, :reloads,
+      :clicks_link_within,
+      :field_labeled
     
   end
 end
