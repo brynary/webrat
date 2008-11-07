ebcef2d073a9c220aca26522f4a572ccbea4935d
Bryan Helmkamp <bryan@brynary.com>
2008-11-07 11:40:05 -0500

    Bumping version

--- lib/webrat.rb
+++ lib/webrat.rb
@@ -3,7 +3,7 @@ require "rubygems"
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__))) unless $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Webrat
-  VERSION = '0.2.2'
+  VERSION = '0.3.0'
   
   def self.root
     defined?(RAILS_ROOT) ? RAILS_ROOT : Merb.root
