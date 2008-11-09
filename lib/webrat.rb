859a15b8d8f6253183986caf832477bacf7dcc9a
Bryan Helmkamp <bryan@brynary.com>
2008-11-08 21:38:56 -0500

    Prep for the 0.3.2 release

--- lib/webrat.rb
+++ lib/webrat.rb
@@ -3,7 +3,7 @@ require "rubygems"
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__))) unless $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Webrat
-  VERSION = '0.3.1'
+  VERSION = '0.3.2'
   
   def self.root #:nodoc:
     defined?(RAILS_ROOT) ? RAILS_ROOT : Merb.root
