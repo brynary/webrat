7ef8fdf7ba0344231c2dae666ecfcb2d1ebedfb1
Bryan Helmkamp <bryan@brynary.com>
2008-11-27 00:40:18 -0500

    Processing CSS searches as XPath

--- lib/webrat/core/xml.rb
+++ lib/webrat/core/xml.rb
@@ -25,14 +25,27 @@ module Webrat #:nodoc:
       end
     end
     
+    def self.xpath_search(element, *searches)
+      searches.flatten.map do |search|
+        element.xpath(search)
+      end.flatten.compact
+    end
+    
     def self.css_search(element, *searches) #:nodoc:
       if Webrat.configuration.parse_with_nokogiri?
-        element.css(*searches)
+        xpath_search(element, css_to_xpath(*searches))
+        # element.css(*searches)
       else
         searches.map do |search|
           element.search(search)
         end.flatten.compact
       end
+    end
+    
+    def self.css_to_xpath(*selectors)
+      selectors.map do |rule|
+        Nokogiri::CSS.xpath_for(rule, :prefix => ".//")
+      end.flatten.uniq
     end
     
   end
