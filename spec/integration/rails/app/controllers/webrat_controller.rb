add38820e58cba6a7e1ee62155c29480a9d1d913
Bryan Helmkamp <bryan@brynary.com>
2008-12-29 01:30:26 -0500

    Adding two tests for Rails integration

---
+++ spec/integration/rails/app/controllers/webrat_controller.rb
@@ -0,0 +1,10 @@
+class WebratController < ApplicationController
+  
+  def form
+  end
+  
+  def submit
+    render :text => "OK"
+  end
+  
+end
--- spec/integration/rails/test/integration/webrat_test.rb
+++ spec/integration/rails/test/integration/webrat_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class WebratTest < ActionController::IntegrationTest
-  test "should pass" do
-    assert true
+  test "should visit pages" do
+    visit root_path
+    assert_tag "Webrat Form"
+    assert response.body.include?("Webrat Form")
+  end
+  
+  test "should submit forms" do
+    visit root_path
+    fill_in "Text field", :with => "Hello"
+    check "TOS"
+    select "January"
+    click_button "Test"
   end
 end
