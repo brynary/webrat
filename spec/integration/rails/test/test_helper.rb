7258ea3349d3d90a38f64593b1cd35ff6f9306fd
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2008-12-29 06:41:30 -0200

    Make rails integration test load 'local' webrat, instead of the gem.

--- spec/integration/rails/test/test_helper.rb
+++ spec/integration/rails/test/test_helper.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
 require "redgreen"
 
-require "webrat"
+require File.dirname(__FILE__) + "/../../../../lib/webrat"
 
 Webrat.configure do |config|
   config.mode = :rails
