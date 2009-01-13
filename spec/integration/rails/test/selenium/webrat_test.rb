758e323f89fd122543fcdb7fac9c90061bd0c19b
Mike Gaffney <mike@uberu.com>
2009-01-13 00:26:49 -0600

    breaking up selenium from rails_webrat tests for the moment

---
+++ spec/integration/rails/test/selenium/webrat_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class WebratTest < ActionController::IntegrationTest
+  test "should visit pages" do
+    visit root_path
+    assert_contain("Webrat Form")
+  end
+
+  test "should submit forms" do
+    visit root_path
+    fill_in "Text field", :with => "Hello"
+    check "TOS"
+    select "January"
+    click_button "Test"
+  end
+
+  test "should follow internal redirects" do
+    visit internal_redirect_path
+    assert response_body.include?("OK")
+  end
+  
+  test "should not follow external redirects" do
+    visit external_redirect_path
+    assert response.redirect?
+  end
+end
