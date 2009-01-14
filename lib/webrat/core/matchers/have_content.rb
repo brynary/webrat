ea930106b6b42c44c7bd588078199b84a3f4420b
Amos King <amos.l.king@gmail.com>
2009-01-13 22:14:49 -0600

    remove commented code and make assert_contain use a true assertion

--- lib/webrat/core/matchers/have_content.rb
+++ lib/webrat/core/matchers/have_content.rb
@@ -55,14 +55,14 @@ module Webrat
     # the supplied string or regexp
     def assert_contain(content)
       hc = HasContent.new(content)
-      raise Test::Unit::AssertionFailedError.new(hc.failure_message) unless hc.matches?(response_body)
+      assert hc.matches?(response_body), hc.failure_message
     end
     
     # Asserts that the body of the response
     # does not contain the supplied string or regepx
     def assert_not_contain(content)
       hc = HasContent.new(content)
-      raise Test::Unit::AssertionFailedError.new(hc.negative_failure_message) if hc.matches?(response_body)
+      assert !hc.matches?(response_body), hc.negative_failure_message
     end
     
   end
