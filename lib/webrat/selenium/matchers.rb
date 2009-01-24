3a5d34e7592d44bf55b962a7522f3b85e4e791f0
Amos King <amos.l.king@gmail.com>
2009-01-23 21:01:21 -0600

    Moved have selector

--- lib/webrat/selenium/matchers.rb
+++ lib/webrat/selenium/matchers.rb
@@ -1,53 +1,11 @@
 require "webrat/selenium/matchers/have_xpath"
-# require "webrat/core/matchers/have_selector"
+require "webrat/selenium/matchers/have_selector"
 # require "webrat/core/matchers/have_tag"
 # require "webrat/core/matchers/have_content"
 
 module Webrat
   module Selenium
     module Matchers
-      class HaveSelector
-        def initialize(expected)
-          @expected = expected
-        end
-        
-        def matches?(response)
-          response.session.wait_for do
-            response.selenium.is_element_present("css=#{@expected}")
-          end
-        end
-
-        # ==== Returns
-        # String:: The failure message.
-        def failure_message
-          "expected following text to match selector #{@expected}:\n#{@document}"
-        end
-
-        # ==== Returns
-        # String:: The failure message to be displayed in negative matches.
-        def negative_failure_message
-          "expected following text to not match selector #{@expected}:\n#{@document}"
-        end
-      end
-      
-      def have_selector(content)
-        HaveSelector.new(content)
-      end
-      
-      # Asserts that the body of the response contains
-      # the supplied selector
-      def assert_have_selector(expected)
-        hs = HaveSelector.new(expected)
-        assert hs.matches?(response), hs.failure_message
-      end
-
-      # Asserts that the body of the response
-      # does not contain the supplied string or regepx
-      def assert_have_no_selector(expected)
-        hs = HaveSelector.new(expected)
-        assert !hs.matches?(response), hs.negative_failure_message
-      end
-      
       class HasContent #:nodoc:
         def initialize(content)
           @content = content
