49e86d103f33f39ae8f96af7ce834e5ab0310d48
Noah Davis <noah@weplay.com>
2009-01-28 14:34:24 -0500

    comment out have_tag from selenium matcher which overrides rails' have_tag
    partially reverts 483559f279a773c931d20a00f42af6a2393013b3

--- lib/webrat/selenium/matchers.rb
+++ lib/webrat/selenium/matchers.rb
@@ -1,4 +1,4 @@
 require "webrat/selenium/matchers/have_xpath"
 require "webrat/selenium/matchers/have_selector"
-require "webrat/selenium/matchers/have_tag"
+# require "webrat/selenium/matchers/have_tag"
 require "webrat/selenium/matchers/have_content"
