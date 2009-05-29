6cd734aec9c701e0b8db605ba32a895487c6f7ad
Matthias Marschall <mm@agileweboperations.com>
2009-05-29 10:24:51 +0200

    made selenium integration tests run applying larrytheliquid's patch: http://github.com/larrytheliquid/webrat/commit/627913708c563f136f17d7da1b85b961f20f143b

--- lib/webrat/selenium.rb
+++ lib/webrat/selenium.rb
@@ -1,13 +1,7 @@
 require "webrat"
 gem "selenium-client", ">=1.2.14"
 require "selenium/client"
-
-# active_support already defines silence_stream, no need to do that again if it's already present.
-# http://github.com/rails/rails/blob/master/activesupport/lib/active_support/core_ext/kernel/reporting.rb
-unless Kernel.respond_to?(:silence_stream)
-  require "webrat/selenium/silence_stream"
-end
-
+require "webrat/selenium/silence_stream"
 require "webrat/selenium/selenium_session"
 require "webrat/selenium/matchers"
 require "webrat/core_extensions/tcp_socket"
