bd1655c8e26c9e1d4f86fda2b7c7fa2a7ffc1b16
mike.gaffney <mike.gaffney@asolutions.com>
2009-06-02 17:23:12 -0500

    [#257 state:awaiting-patch] here is a test proving the issue

---
+++ spec/integration/rails/test/integration/fill_in_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class FillInTest < ActionController::IntegrationTest
+  test "should fill in text field by name" do
+    visit fields_path
+    fill_in "field_by_name", :with => "value"
+  end
+  test "should fill in text field by name, rails naming lh257" do
+    visit fields_path
+    fill_in "rails[naming]", :with => "value"
+  end
+  test "should fill in text field by id" do
+    visit fields_path
+    fill_in "field_by_id", :with => "value"
+  end
+  test "should fill in text field by label via id" do
+    visit fields_path
+    fill_in "FieldByLabelId", :with => "value"
+  end
+end
