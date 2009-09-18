63e1053afaa76d9276df137f042a779447376326
Bryan Helmkamp <bryan@brynary.com>
2009-09-17 20:28:19 -0400

    Fix bug in Selenium when dealing with special characters in link text

--- lib/webrat/selenium/selenium_session.rb
+++ lib/webrat/selenium/selenium_session.rb
@@ -79,7 +79,12 @@ module Webrat
     webrat_deprecate :clicks_button, :click_button
 
     def click_link(link_text_or_regexp, options = {})
-      pattern = adjust_if_regexp(link_text_or_regexp)
+      if link_text_or_regexp.is_a?(Regexp)
+        pattern = "evalregex:#{link_text_or_regexp.inspect}"
+      else
+        pattern = link_text_or_regexp.to_s
+      end
+
       locator = "webratlink=#{pattern}"
       selenium.wait_for_element locator, :timeout_in_seconds => 5
       selenium.click locator
