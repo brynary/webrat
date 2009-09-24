9a92afedff2511148ae02ded69ac241ff1a3b1e1
David Chelimsky <dchelimsky@gmail.com>
2009-09-23 23:04:43 -0500

    require 'nokogiri' in rspec-rails include file

--- lib/webrat/integrations/rspec-rails.rb
+++ lib/webrat/integrations/rspec-rails.rb
@@ -1,8 +1,9 @@
 # Supports using the matchers in controller, helper, and view specs if you're
 # using rspec-rails. Just add a require statement to spec/spec_helper.rb or env.rb:
 #
-#   require 'webrat/rspec-rails'
+#   require 'webrat/integrations/rspec-rails'
 #
+require "nokogiri"
 require "webrat/core/matchers"
 
 Spec::Runner.configure do |config|
