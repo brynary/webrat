5c382a5378f4fd9e76812069b783fcaeb27abac7
Bryan Helmkamp <bryan@brynary.com>
2010-07-24 13:32:38 -0400

    Code formatting

--- lib/webrat/selenium/selenium_session.rb
+++ lib/webrat/selenium/selenium_session.rb
@@ -236,8 +236,12 @@ EOS
 
 
     def create_browser
-      $browser = ::Selenium::Client::Driver.new(Webrat.configuration.selenium_server_address || "localhost",
-      Webrat.configuration.selenium_server_port, Webrat.configuration.selenium_browser_key, "http://#{Webrat.configuration.application_address}:#{Webrat.configuration.application_port_for_selenium}")
+      $browser = ::Selenium::Client::Driver.new(
+        Webrat.configuration.selenium_server_address || "localhost",
+        Webrat.configuration.selenium_server_port,
+        Webrat.configuration.selenium_browser_key,
+        "http://#{Webrat.configuration.application_address}:#{Webrat.configuration.application_port_for_selenium}"
+      )
       $browser.set_speed(0) unless Webrat.configuration.selenium_server_address
 
       at_exit do
