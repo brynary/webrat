c2254b3e01b38b12e55d686c75006529cb421a2c
Alex Eagle <eagle.alex@gmail.com>
2011-04-27 13:22:17 +0400

    Refactor Webrat::XML.document

--- lib/webrat/core/xml.rb
+++ lib/webrat/core/xml.rb
@@ -11,37 +11,9 @@ module Webrat #:nodoc:
       elsif Nokogiri::XML::NodeSet === stringlike
         stringlike
       elsif stringlike.respond_to?(:body)
-        Nokogiri::HTML(stringlike.body.to_s)
+        Nokogiri.parse(stringlike.body.to_s)
       else
-        Nokogiri::HTML(stringlike.to_s)
-      end
-    end
-
-    def self.html_document(stringlike) #:nodoc:
-      return stringlike.dom if stringlike.respond_to?(:dom)
-
-      if Nokogiri::HTML::Document === stringlike
-        stringlike
-      elsif Nokogiri::XML::NodeSet === stringlike
-        stringlike
-      elsif stringlike.respond_to?(:body)
-        Nokogiri::HTML(stringlike.body.to_s)
-      else
-        Nokogiri::HTML(stringlike.to_s)
-      end
-    end
-
-    def self.xml_document(stringlike) #:nodoc:
-      return stringlike.dom if stringlike.respond_to?(:dom)
-
-      if Nokogiri::HTML::Document === stringlike
-        stringlike
-      elsif Nokogiri::XML::NodeSet === stringlike
-        stringlike
-      elsif stringlike.respond_to?(:body)
-        Nokogiri::XML(stringlike.body.to_s)
-      else
-        Nokogiri::XML(stringlike.to_s)
+        Nokogiri.parse(stringlike.to_s)
       end
     end
 
