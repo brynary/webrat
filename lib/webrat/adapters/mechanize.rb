daf8ad2387e082edbe0d6c196d739f1057ebe036
Andreas Knifh <andreas.knifh@jadestone.se>
2011-09-15 06:13:57 +0200

    Added config option to enable follow redirects. Used by mechanize adapter.

--- lib/webrat/adapters/mechanize.rb
+++ lib/webrat/adapters/mechanize.rb
@@ -54,7 +54,7 @@ module Webrat #:nodoc:
     def mechanize
       @mechanize ||= begin
         mechanize = Mechanize.new
-        mechanize.redirect_ok = false
+        mechanize.redirect_ok = Webrat.configuration.follow_redirects
         mechanize
       end
     end
